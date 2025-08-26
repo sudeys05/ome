51cb4609df55d131c9ed8b342bc9d7067f8dd2b2
sudeys05 <sudeys05mn@gmail.com>
2025-08-26 08:35:48 +0300

    j

---
+++ server/custom-types.d.ts
@@ -0,0 +1,13 @@
+declare module 'express';
+declare module 'express-session';
+declare module 'cors';
+declare module 'multer';
+
+declare module './mongodb-connection.js';
+declare module './mongodb-routes.js';
+declare module './evidence-routes.js';
+declare module './custodial-routes.js';
+declare module './vite.js';
+declare module './seed-geofiles.js';
+declare module './seed-admin.js';
+declare module './api-routes.js';
